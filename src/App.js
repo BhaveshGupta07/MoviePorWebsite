d553dbc4583b649c0243f53ccb9e30527d174b09
D3ffyduck <bhuvi007@protonmail.com>
2024-08-17 17:04:00 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homepage from "./views/Homepage";
+import MovieDetail from "./views/DetailsPage/MovieDetail";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TvShowDetail from "./views/DetailsPage/TvShowDetail";
+import SearchPage from "./views/SearchResultPage/SearchPage";
+import CelebrityDetail from "./views/DetailsPage/CelebrityDetail";
+import PopularMovie from "./views/PopularPage/Movie";
+import PopularTv from "./views/PopularPage/TvShow";
+import PopularPeople from "./views/PeoplePage/PopularPeople";
+import TopRatedMovie from "./views/TopRatedPage/Movie";
+import TopRatedTv from "./views/TopRatedPage/TvShow";
+import Upcoming from "./views/UpcomingPage/UpcomingMovies";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="main">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route exact path="/popular-movies" element={<PopularMovie />} />
+          <Route exact path="/popular-tv" element={<PopularTv />} />
+          <Route exact path="/popular-people" element={<PopularPeople />} />
+          <Route exact path="/top-movies" element={<TopRatedMovie />} />
+          <Route exact path="/top-tv" element={<TopRatedTv />} />
+          <Route exact path="/upcoming-movies" element={<Upcoming />} />
+          <Route exact path="/movie/:movieId" element={<MovieDetail />} />
+          <Route exact path="/tv/:tvId" element={<TvShowDetail />} />
+          <Route exact path="/person/:personId" element={<CelebrityDetail />} />
+          <Route exact path="/search/:searchTerm" element={<SearchPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,57 @@
+import { GitHub, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-section">
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="copyright">
+            <Link to="/">
+              <img
+                src={require("../../assets/images/logo.png")}
+                alt="MoviePorium-Logo"
+                className="footer-logo"
+              />
+            </Link>
+            <p className="copy-p">Copyright Bhavesh gupta &copy; 2024</p>
+          </div>
+          <nav className="footer-nav">
+            <ul>
+              <li>
+                <Link to="/popular-movies?page=1">Popular</Link>
+              </li>
+              <li>
+                <Link to="top-movies?page=1">Top Rated</Link>
+              </li>
+              <li>
+                <Link to="popular-people?page=1">People</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="social">
+            <a href="https://github.com/BhaveshGupta07" target="_blank">
+              <GitHub fontSize="large" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/bhavesh-gupta07/"
+              target="_blank"
+            >
+              <LinkedIn fontSize="large" />
+            </a>
+            <a href="/" target="_blank">
+              <Instagram fontSize="large" />
+            </a>
+            <a href="/" target="_blank">
+              <Twitter fontSize="large" />
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
